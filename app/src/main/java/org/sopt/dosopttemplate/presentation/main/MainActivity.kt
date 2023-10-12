b06892d5170bd57e38978b6253958a787a9bb275
jihyunniiii <jita1125@naver.com>
2023-10-12 11:14:16 +0900

    [feat] #1 main 뷰 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -1,12 +1,29 @@
 package org.sopt.dosopttemplate.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.ActivityMainBinding
+import org.sopt.dosopttemplate.presentation.auth.SignUpActivity.Companion.USER_INFO
+import org.sopt.dosopttemplate.presentation.model.UserInfo
+import org.sopt.dosopttemplate.util.binding.BindingActivity
+import org.sopt.dosopttemplate.util.extension.getCompatibleParcelableExtra
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.lifecycleOwner = this
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        intent.getCompatibleParcelableExtra<UserInfo>(USER_INFO)?.let { userInfo ->
+            with(binding) {
+                tvMainNickname.text = userInfo.nickname
+                tvMainId.text = userInfo.id
+                tvMainMbti.text = userInfo.mbti
+            }
+        }
     }
 }
