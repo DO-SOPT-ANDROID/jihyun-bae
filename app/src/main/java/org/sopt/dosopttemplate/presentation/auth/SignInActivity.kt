04282ff86912c68d86a02e042e27b410c0ad484a
jihyunniiii <jita1125@naver.com>
2023-10-12 10:35:06 +0900

    [feat] #1 sign in 기능 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignInActivity.kt
@@ -0,0 +1,73 @@
+package org.sopt.dosopttemplate.presentation.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.ActivitySignInBinding
+import org.sopt.dosopttemplate.presentation.auth.SignUpActivity.Companion.USER_INFO
+import org.sopt.dosopttemplate.presentation.main.MainActivity
+import org.sopt.dosopttemplate.presentation.model.UserInfo
+import org.sopt.dosopttemplate.util.binding.BindingActivity
+import org.sopt.dosopttemplate.util.extension.getCompatibleParcelableExtra
+import org.sopt.dosopttemplate.util.extension.showSnackbar
+import org.sopt.dosopttemplate.util.extension.showToast
+
+class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    private val viewModel: AuthViewModel by viewModels()
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        setActivityResultLauncher()
+        addListeners()
+    }
+
+    private fun setActivityResultLauncher() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+                if (activityResult.resultCode == RESULT_OK) {
+                    viewModel.userInfo =
+                        activityResult.data?.getCompatibleParcelableExtra<UserInfo>(USER_INFO)
+                            ?: return@registerForActivityResult
+                    binding.root.showSnackbar(getString(R.string.sign_up_success))
+                }
+            }
+    }
+
+    private fun addListeners() {
+        binding.btnSignInSignIn.setOnClickListener {
+            if (viewModel.isSignInEnabled(
+                    binding.etSignInId.text.toString(),
+                    binding.etSignInPassword.text.toString()
+                )
+            ) {
+                showToast(getString(R.string.sign_in_success))
+                moveToMain()
+            } else {
+                showToast(getString(R.string.sign_in_failed))
+            }
+        }
+
+        binding.tvSignInSignUp.setOnClickListener {
+            moveToSignUp()
+        }
+    }
+
+    private fun moveToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(USER_INFO, viewModel.userInfo)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun moveToSignUp() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        resultLauncher.launch(intent)
+    }
+}
