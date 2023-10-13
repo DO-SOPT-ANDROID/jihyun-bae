fcea76c6d91d8bd63117138d5758a81b1bf22ebb
jihyunniiii <jita1125@naver.com>
2023-10-13 19:50:18 +0900

    [feat] #5 DoubleBackPress 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/MainActivity.kt
@@ -1,12 +1,26 @@
 package org.sopt.dosopttemplate.presentation.main
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.datasource.local.DoSoptDataSource
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 import org.sopt.dosopttemplate.util.binding.BindingActivity
+import org.sopt.dosopttemplate.util.extension.showToast
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private var backPressedTime = INIT_BACK_PRESSED_TIME
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime <= DELAY_TIME) {
+                finish()
+            } else {
+                backPressedTime = System.currentTimeMillis()
+                showToast(getString(R.string.double_back_press_message))
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,6 +28,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
         initLayout()
         addListeners()
+        finishOnDoubleBackPress()
     }
 
     private fun initLayout() {
@@ -31,11 +46,17 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         }
     }
 
-    private fun showLogoutDialog(){
+    private fun showLogoutDialog() {
         LogoutDialog().show(supportFragmentManager, DIALOG)
+    }
+
+    private fun finishOnDoubleBackPress() {
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     companion object {
         const val DIALOG = "dialog"
+        const val INIT_BACK_PRESSED_TIME = 0L
+        const val DELAY_TIME = 2000
     }
 }
