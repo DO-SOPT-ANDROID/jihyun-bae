56957f2820971f212b196c84939ef1bc7a2c0c12
jihyunniiii <jita1125@naver.com>
2023-10-13 18:07:12 +0900

    [feat] #5 Intent Flags 설정

--- app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignInActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/auth/SignInActivity.kt
@@ -82,6 +82,7 @@ class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_
 
     private fun moveToMain() {
         val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
         finish()
     }
