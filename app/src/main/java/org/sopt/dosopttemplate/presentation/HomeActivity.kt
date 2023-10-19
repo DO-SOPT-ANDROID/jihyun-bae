7bda73767c56aa549861e1bbb754c3d2bda03e41
jihyunniiii <jita1125@naver.com>
2023-10-19 20:47:15 +0900

    [feat] #7 HomeActivity 구현

--- app/src/main/java/org/sopt/dosopttemplate/presentation/HomeActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/HomeActivity.kt
@@ -1,17 +1,21 @@
-package org.sopt.dosopttemplate.presentation.main
+package org.sopt.dosopttemplate.presentation
 
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
-import org.sopt.dosopttemplate.databinding.ActivityMainBinding
+import org.sopt.dosopttemplate.databinding.ActivityHomeBinding
+import org.sopt.dosopttemplate.presentation.doAndroid.DoAndroidFragment
+import org.sopt.dosopttemplate.presentation.home.HomeFragment
+import org.sopt.dosopttemplate.presentation.myPage.MyPageFragment
 import org.sopt.dosopttemplate.util.binding.BindingActivity
 import org.sopt.dosopttemplate.util.extension.showToast
 
 @AndroidEntryPoint
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private val viewModel by viewModels<MainViewModel>()
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     private var backPressedTime = INIT_BACK_PRESSED_TIME
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -27,38 +31,32 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.viewModel = viewModel
-
         initLayout()
-        addListeners()
         finishOnDoubleBackPress()
     }
 
     private fun initLayout() {
-        val userInfo = viewModel.getUserInfo()
-        with(binding) {
-            tvMainNickname.text = userInfo.nickname
-            tvMainId.text = userInfo.id
-            tvMainMbti.text = userInfo.mbti
+        binding.bnvHome.setOnItemSelectedListener { menu ->
+            when (menu.itemId) {
+                R.id.menu_do_android -> navigateTo<DoAndroidFragment>()
+                R.id.menu_home -> navigateTo<HomeFragment>()
+                R.id.menu_my_page -> navigateTo<MyPageFragment>()
+            }
+            true
         }
-    }
-
-    private fun addListeners() {
-        binding.tvMainLogout.setOnClickListener {
-            showLogoutDialog()
-        }
-    }
-
-    private fun showLogoutDialog() {
-        LogoutDialog({ viewModel.clearUserDataSource() }).show(supportFragmentManager, DIALOG)
     }
 
     private fun finishOnDoubleBackPress() {
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fcv_home_container, T::class.java.canonicalName)
+        }
+    }
+
     companion object {
-        const val DIALOG = "dialog"
         const val INIT_BACK_PRESSED_TIME = 0L
         const val DELAY_TIME = 2000
     }
