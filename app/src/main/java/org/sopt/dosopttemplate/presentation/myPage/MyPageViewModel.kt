cb8de6c7fff2d69b7ab8aa36b69feeb259c1c15a
jihyunniiii <jita1125@naver.com>
2023-10-19 20:48:12 +0900

    [chore] #7 네이밍 변경

--- app/src/main/java/org/sopt/dosopttemplate/presentation/myPage/MyPageViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/myPage/MyPageViewModel.kt
@@ -1,4 +1,4 @@
-package org.sopt.dosopttemplate.presentation.main
+package org.sopt.dosopttemplate.presentation.myPage
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -6,7 +6,7 @@ import org.sopt.dosopttemplate.domain.repository.UserRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
+class MyPageViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
     fun getUserInfo() = userRepository.getUserInfo()
