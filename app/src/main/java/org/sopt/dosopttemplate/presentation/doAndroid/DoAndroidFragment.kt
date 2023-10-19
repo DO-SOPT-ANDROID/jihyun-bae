2eda85cb543ccfbf9f6abffd909d9bd24cb1e79c
jihyunniiii <jita1125@naver.com>
2023-10-19 20:45:46 +0900

    [feat] #7 Fragment 생성 (DoAndroid, Home, MyPage)

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/doAndroid/DoAndroidFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.dosopttemplate.presentation.doAndroid
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.FragmentDoAndroidBinding
+import org.sopt.dosopttemplate.util.binding.BindingFragment
+
+class DoAndroidFragment : BindingFragment<FragmentDoAndroidBinding>(R.layout.fragment_do_android) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.dosopttemplate.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+import org.sopt.dosopttemplate.util.binding.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
