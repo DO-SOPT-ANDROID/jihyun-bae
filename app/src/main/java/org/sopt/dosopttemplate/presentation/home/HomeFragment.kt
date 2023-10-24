d835c5dc3ac1d1f2392c87969df105b3e3b13b1f
jihyunniiii <jita1125@naver.com>
2023-10-24 10:31:09 +0900

    [feat] #9  Scroll to Top 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
@@ -6,10 +6,11 @@ import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+import org.sopt.dosopttemplate.presentation.type.ScrollableView
 import org.sopt.dosopttemplate.util.binding.BindingFragment
 
 @AndroidEntryPoint
-class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home), ScrollableView {
     private val viewModel by viewModels<HomeViewModel>()
     private lateinit var homeProfileAdapter: HomeProfileAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,9 +21,17 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         initLayout()
     }
 
+    override fun scrollToTop() {
+        binding.rvHome.smoothScrollToPosition(FIRST_POSITION)
+    }
+
     private fun initLayout() {
         homeProfileAdapter = HomeProfileAdapter()
         homeProfileAdapter.submitList(viewModel.getMockProfileList())
         binding.rvHome.adapter = homeProfileAdapter
+    }
+
+    companion object {
+        const val FIRST_POSITION = 0
     }
 }
