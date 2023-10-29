8f5b4405fc44c7feb07d05d003099b59ab12a320
jihyunniiii <jita1125@naver.com>
2023-10-29 13:41:59 +0900

    [feat] #11 ProfileDetail 페이지 구현

--- app/src/main/java/org/sopt/dosopttemplate/domain/model/Profile.kt
+++ app/src/main/java/org/sopt/dosopttemplate/domain/model/Profile.kt
@@ -8,14 +8,26 @@ sealed class Profile {
         val profileImage: Int,
         val name: String,
         val description: String?,
-    ) : Profile()
+    ) : Profile() {
+        fun toMyProfile() = org.sopt.dosopttemplate.presentation.model.Profile.MyProfile(
+            profileImage = this.profileImage,
+            name = this.name,
+            description = this.description
+        )
+    }
 
     data class FriendProfile(
         @DrawableRes
         val profileImage: Int,
         val name: String,
         val description: String?
-    ) : Profile()
+    ) : Profile() {
+        fun toFriendProfile() = org.sopt.dosopttemplate.presentation.model.Profile.FriendProfile(
+            profileImage = this.profileImage,
+            name = this.name,
+            description = this.description
+        )
+    }
 
     data class FriendProfileWithMusic(
         @DrawableRes
@@ -24,7 +36,16 @@ sealed class Profile {
         val description: String?,
         val musicTitle: String,
         val singer: String
-    ) : Profile()
+    ) : Profile() {
+        fun toFriendProfileWithMusic() =
+            org.sopt.dosopttemplate.presentation.model.Profile.FriendProfileWithMusic(
+                profileImage = this.profileImage,
+                name = this.name,
+                description = this.description,
+                musicTitle = this.musicTitle,
+                singer = this.singer
+            )
+    }
 
     data class FriendProfileWithBirth(
         @DrawableRes
@@ -32,5 +53,13 @@ sealed class Profile {
         val name: String,
         val birthMonth: Int,
         val birthDay: Int
-    ) : Profile()
+    ) : Profile() {
+        fun toFriendProfileWithBirth() =
+            org.sopt.dosopttemplate.presentation.model.Profile.FriendProfileWithBirth(
+                profileImage = this.profileImage,
+                name = this.name,
+                birthMonth = this.birthMonth,
+                birthDay = this.birthDay
+            )
+    }
 }
