67519cd2e4288eeb7f87d3efd5db20c6648312b2
jihyunniiii <jita1125@naver.com>
2023-11-01 08:54:04 +0900

    [feat] #11 가로모드 구현으로 인한 네이밍 수정 및 Type 수정

--- app/src/main/java/org/sopt/dosopttemplate/presentation/type/ProfileInfoType.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/type/ProfileInfoType.kt
@@ -5,22 +5,26 @@ import androidx.annotation.StringRes
 import org.sopt.dosopttemplate.R
 
 enum class ProfileInfoType(
-    @DrawableRes val backgroundRes: Int,
+    @DrawableRes val portraitBackgroundRes: Int,
+    @DrawableRes val landscapeBackgroundRes: Int,
     @StringRes val contextRes: Int?,
     @DrawableRes val iconRes: Int
 ) {
     DESCRIPTION(
-        backgroundRes = R.drawable.shape_gray_300_line_12_rect,
+        portraitBackgroundRes = R.drawable.shape_gray_300_line_12_rect,
+        landscapeBackgroundRes = R.drawable.shape_gray_300_line_50_rect,
         contextRes = R.string.home_profile_info_type_description,
         iconRes = R.drawable.ic_add_gray_400_24
     ),
     MUSIC(
-        backgroundRes = R.drawable.shape_main_4_line_12_rect,
+        portraitBackgroundRes = R.drawable.shape_main_4_line_12_rect,
+        landscapeBackgroundRes = R.drawable.shape_main_4_line_50_rect,
         contextRes = null,
         iconRes = R.drawable.ic_music_main_4_24
     ),
     BIRTH(
-        backgroundRes = R.drawable.shape_point_3_line_12_rect,
+        portraitBackgroundRes = R.drawable.shape_point_3_line_12_rect,
+        landscapeBackgroundRes = R.drawable.shape_point_3_line_50_rect,
         contextRes = R.string.home_profile_info_type_birth,
         iconRes = R.drawable.ic_gift_point_3_24
     )
