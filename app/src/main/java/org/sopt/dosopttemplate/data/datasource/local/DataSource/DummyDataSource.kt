bbc01062d2b9301063439d5fc6662724f4f32517
jihyunniiii <jita1125@naver.com>
2023-11-03 21:49:12 +0900

    [del] #11 사용하지 않는 파일 제거 (이미지 파일 및 더미 데이터)

--- app/src/main/java/org/sopt/dosopttemplate/data/datasource/local/DataSource/DummyDataSource.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.dosopttemplate.data.datasource.local.DataSource
-
-import org.sopt.dosopttemplate.data.model.local.Profile
-
-interface DummyDataSource {
-    val mockProfileList: List<Profile>
-}
--- app/src/main/java/org/sopt/dosopttemplate/domain/repository/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.dosopttemplate.domain.repository
-
-import org.sopt.dosopttemplate.domain.model.Profile
-
-interface DummyRepository {
-    fun getMockProfileList(): List<Profile>
-}
