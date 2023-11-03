da7a9e3bd6a05a1b1e734837d228e7ec1da497c5
jihyunniiii <jita1125@naver.com>
2023-11-03 21:50:40 +0900

    [feat] #11 Room DB 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/data/model/local/ProfileEntity.kt
@@ -0,0 +1,60 @@
+package org.sopt.dosopttemplate.data.model.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.sopt.dosopttemplate.domain.model.Profile
+
+@Entity(tableName = "profile_table")
+data class ProfileEntity(
+    @PrimaryKey
+    val id: Int,
+    @ColumnInfo(name = "profile_type")
+    val profileType: ProfileType,
+    @ColumnInfo(name = "profile_image")
+    val profileImage: String,
+    val name: String,
+    val description: String? = null,
+    val musicTitle: String? = null,
+    val singer: String? = null,
+    val birthMonth: Int? = null,
+    val birthDay: Int? = null
+) {
+    enum class ProfileType {
+        MY_PROFILE,
+        FRIEND_PROFILE,
+        FRIEND_PROFILE_WITH_MUSIC,
+        FRIEND_PROFILE_WITH_BIRTH
+    }
+
+    fun toMyProfile() = Profile.MyProfile(
+        id = this.id,
+        profileImage = this.profileImage,
+        name = this.name,
+        description = this.description
+    )
+
+    fun toFriendProfile() = Profile.FriendProfile(
+        id = this.id,
+        profileImage = this.profileImage,
+        name = this.name,
+        description = this.description
+    )
+
+    fun toFriendProfileWithMusic() = Profile.FriendProfileWithMusic(
+        id = this.id,
+        profileImage = this.profileImage,
+        name = this.name,
+        description = this.description,
+        musicTitle = musicTitle ?: "",
+        singer = singer ?: ""
+    )
+
+    fun toFriendProfileWithBirth() = Profile.FriendProfileWithBirth(
+        id = this.id,
+        profileImage = this.profileImage,
+        name = this.name,
+        birthMonth = this.birthMonth ?: 0,
+        birthDay = this.birthDay ?: 0
+    )
+}
