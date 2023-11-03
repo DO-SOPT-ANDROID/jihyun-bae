65dba3ccb4c0653cc5a3cf255078f92dfe31f5a7
jihyunniiii <jita1125@naver.com>
2023-11-03 21:52:04 +0900

    [feat] #11 getProfile 구현

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewModel.kt
@@ -1,13 +1,32 @@
 package org.sopt.dosopttemplate.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import org.sopt.dosopttemplate.domain.repository.DummyRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.sopt.dosopttemplate.domain.model.Profile
+import org.sopt.dosopttemplate.domain.repository.ProfileRoomRepository
+import org.sopt.dosopttemplate.util.UiState
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val dummyRepository: DummyRepository,
+    private val profileRoomRepository: ProfileRoomRepository
 ) : ViewModel() {
-    fun getMockProfileList() = dummyRepository.getMockProfileList()
+    private val _profileListState = MutableStateFlow<UiState<List<Profile>>>(UiState.Loading)
+    val profileListState = _profileListState.asStateFlow()
+
+    fun getProfileList() {
+        viewModelScope.launch {
+            profileRoomRepository.getProfileList()
+                .onSuccess { profileList ->
+                    _profileListState.value = UiState.Success(profileList)
+                }
+                .onFailure { throwable ->
+                    _profileListState.value = UiState.Error(throwable.message)
+                }
+        }
+    }
 }
