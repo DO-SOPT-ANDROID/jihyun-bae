5053526bca087ce0ec497b7a816e510467448d48
jihyunniiii <jita1125@naver.com>
2023-12-13 20:52:56 +0900

    [feat] #22 BindingAdapter 적용

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewHolder.kt
@@ -2,7 +2,6 @@ package org.sopt.dosopttemplate.presentation.home
 
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.databinding.ItemHomeUserBinding
 import org.sopt.dosopttemplate.domain.model.ReqresUser
@@ -12,8 +11,8 @@ class HomeViewHolder(
     private val context: Context
 ) : RecyclerView.ViewHolder(binding.root) {
     fun onBind(reqresUser: ReqresUser) {
+        binding.reqresUser = reqresUser
         with(binding) {
-            ivHomeUserProfile.load(reqresUser.avatar)
             tvHomeProfileName.text =
                 context.getString(R.string.home_name, reqresUser.firstName, reqresUser.lastName)
             tvHomeProfileEmail.text = reqresUser.email
